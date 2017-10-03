298f2d23cca702918dcfba214c07c50c9a7d4b7f
Nevin Liber <nevin@cplusplusguy.com>
2017-10-03 11:26:17 -0500

    Added chrono.h

---
+++ chrono.h
@@ -0,0 +1,41 @@
+#ifndef COOL_CHRONO_H_
+#define COOL_CHRONO_H_
+
+#include <cool/ratio.h>
+#include <chrono>
+#include <ostream>
+
+///////////////////////////////////////////////////////////////////////////////
+// A stream inserter operator for
+//  std::chrono::duration
+//
+// Usage:
+//  using cool::chrono::operator<<;
+//  std::cout << std::chrono::hours{2} << std::endl;  // outputs "2 hours"
+///////////////////////////////////////////////////////////////////////////////
+namespace cool { namespace chrono {
+
+    template<typename Rep, intmax_t N, intmax_t D>
+    std::ostream& operator<<(std::ostream& os, std::chrono::duration<Rep, std::ratio<N, D>> const& that)
+    {
+        os << that.count() << ' ';
+
+        if (N == 3600 && D == 1)
+            os << "hours";
+        else if (N == 60 && D == 1)
+            os << "minutes";
+        else if (N == 1 && D == 1)
+            os << "seconds";
+        else
+        {
+            using cool::ratio::operator<<;
+            os << std::ratio<N, D>{} << "seconds";
+        }
+
+        return os;
+    }
+
+} /* chrono namespace */ } /* cool namespace */
+
+#endif /* COOL_CHRONO_H_ */
+
