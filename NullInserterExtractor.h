c77e8b72e0bed303113bdc6927b75c440125fb8a
Nevin Liber <nevin@cplusplusguy.com>
2017-10-04 16:16:42 -0500

    Made NullInserterExtractor ops constexpr and noexcept (since all they do is return a reference)

--- NullInserterExtractor.h
+++ NullInserterExtractor.h
@@ -18,13 +18,13 @@ namespace cool
     class NullInserterExtractor
     {
         template<typename charT, typename traits>
-        friend std::basic_istream<charT, traits>&
-        operator>>(std::basic_istream<charT, traits>& is, NullInserterExtractor)
+        friend constexpr std::basic_istream<charT, traits>&
+        operator>>(std::basic_istream<charT, traits>& is, NullInserterExtractor) noexcept
         { return is; }
 
         template<typename charT, typename traits>
-        friend std::basic_ostream<charT, traits>&
-        operator<<(std::basic_ostream<charT, traits>& os, NullInserterExtractor)
+        friend constexpr std::basic_ostream<charT, traits>&
+        operator<<(std::basic_ostream<charT, traits>& os, NullInserterExtractor) noexcept
         { return os; }
     };
 
