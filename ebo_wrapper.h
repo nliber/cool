b2dd3ce3cc44d76c0da7639e735c4b06c7351375
Nevin Liber <nevin@cplusplusguy.com>
2017-10-17 14:24:47 -0500

    Switched SFINAE condition on ebo_wrapper to only use private inheritance when the the type is empty and non-final.
    This takes into account someone marking their virtual
    destructors as final.

--- ebo_wrapper.h
+++ ebo_wrapper.h
@@ -8,9 +8,10 @@
 ///////////////////////////////////////////////////////////////////////////////
 // ebo_wrapper
 //
-//  ebo_wrapper is a wrapper which uses private inheritance where it can and
-//  aggregation where it must.  It's key benefit is to provide a consistent
-//  interface regardless of which implementation is used.
+//  ebo_wrapper is a wrapper which uses private inheritance to take advantage
+//  of EBO for non-final classes and aggregation everywhere else.  The 
+//  key benefit is to provide a consistent interface regardless of which
+//  implementation is used.
 //
 //  The intended use is to privately inherit from ebo_wrapper<T> and call
 //  ref() to get a reference to the held T.
@@ -32,46 +33,6 @@ namespace cool
     {
         template<typename T, typename = void>
         struct ebo_wrapper
-        : private T
-        {
-            using value_type      = T;
-            using reference       = T      &;
-            using const_reference = T const&;
-
-            constexpr ebo_wrapper() = default;
-
-            template<typename U, typename = std::enable_if_t<!std::is_convertible_v<U, ebo_wrapper>>>
-            constexpr ebo_wrapper(U&& u)
-            noexcept(noexcept(T(std::forward<U>(u))))
-            : T(std::forward<U>(u))
-            {}
-
-            template<typename U0, typename U1, typename... Us>
-            constexpr ebo_wrapper(U0&& u0, U1&& u1, Us&&... us)
-            noexcept(noexcept(T(std::forward<U0>, std::forward<U1>, std::forward<Us>(us)...)))
-            : T(std::forward<U0>(u0), std::forward<U1>(u1), std::forward<Us>(us)...)
-            {}
-
-            constexpr reference       ref()       noexcept { return *this; }
-            constexpr const_reference ref() const noexcept { return *this; }
-
-            constexpr operator       reference()       noexcept { return *this; }
-            constexpr operator const_reference() const noexcept { return *this; }
-
-            template<typename... ArgTypes>
-            auto // std::invoke_result_t<T&, ArgTypes...>
-            operator()(ArgTypes&&... args)
-            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
-
-            template<typename... ArgTypes>
-            auto // std::invoke_result_t<T const&, ArgTypes...>
-            operator()(ArgTypes&&... args) const
-            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
-
-        };
-        
-        template<typename T>
-        struct ebo_wrapper<T, std::enable_if_t<!std::is_class_v<T> || std::is_final_v<T>>>
         {
             using value_type      = T;
             using reference       = T      &;
@@ -109,6 +70,46 @@ namespace cool
 
         private:
             T m_t;
+        };
+
+        template<typename T>
+        struct ebo_wrapper<T, std::enable_if_t<std::is_empty_v<T> && !std::is_final_v<T>>>
+        : private T
+        {
+            using value_type      = T;
+            using reference       = T      &;
+            using const_reference = T const&;
+
+            constexpr ebo_wrapper() = default;
+
+            template<typename U, typename = std::enable_if_t<!std::is_convertible_v<U, ebo_wrapper>>>
+            constexpr ebo_wrapper(U&& u)
+            noexcept(noexcept(T(std::forward<U>(u))))
+            : T(std::forward<U>(u))
+            {}
+
+            template<typename U0, typename U1, typename... Us>
+            constexpr ebo_wrapper(U0&& u0, U1&& u1, Us&&... us)
+            noexcept(noexcept(T(std::forward<U0>, std::forward<U1>, std::forward<Us>(us)...)))
+            : T(std::forward<U0>(u0), std::forward<U1>(u1), std::forward<Us>(us)...)
+            {}
+
+            constexpr reference       ref()       noexcept { return *this; }
+            constexpr const_reference ref() const noexcept { return *this; }
+
+            constexpr operator       reference()       noexcept { return *this; }
+            constexpr operator const_reference() const noexcept { return *this; }
+
+            template<typename... ArgTypes>
+            auto // std::invoke_result_t<T&, ArgTypes...>
+            operator()(ArgTypes&&... args)
+            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
+
+            template<typename... ArgTypes>
+            auto // std::invoke_result_t<T const&, ArgTypes...>
+            operator()(ArgTypes&&... args) const
+            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
+
         };
 
     } // detail namespace
