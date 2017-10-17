db02518a1db769ee1ab7899915be16259bad4aae
Nevin Liber <nevin@cplusplusguy.com>
2017-10-17 11:16:39 -0500

    Added a callable interface to ebo_wrapper

--- ebo_wrapper.h
+++ ebo_wrapper.h
@@ -1,6 +1,7 @@
 #ifndef COOL_EBO_WRAPPER_H_
 #define COOL_EBO_WRAPPER_H_
 
+#include <functional>
 #include <type_traits>
 #include <utility>
 
@@ -13,6 +14,9 @@
 //
 //  The intended use is to privately inherit from ebo_wrapper<T> and call
 //  ref() to get a reference to the held T.
+//
+//  Like reference_wrapper, there is also an implicit conversion to
+//  (const_)reference and a Callable interface.
 //
 //  cool::detail::ebo_wrapper takes two template parameters, the second of
 //  which should always be void and is used to SFINAE private inheritance vs.
@@ -53,6 +57,17 @@ namespace cool
 
             constexpr operator       reference()       noexcept { return *this; }
             constexpr operator const_reference() const noexcept { return *this; }
+
+            template<typename... ArgTypes>
+            auto // std::invoke_result_t<T&, ArgTypes...>
+            operator()(ArgTypes&&... args)
+            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
+
+            template<typename... ArgTypes>
+            auto // std::invoke_result_t<T const&, ArgTypes...>
+            operator()(ArgTypes&&... args) const
+            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
+
         };
         
         template<typename T>
@@ -81,6 +96,16 @@ namespace cool
 
             constexpr operator       reference()       noexcept { return m_t; }
             constexpr operator const_reference() const noexcept { return m_t; }
+
+            template<typename... ArgTypes>
+            auto // std::invoke_result_t<T&, ArgTypes...>
+            operator()(ArgTypes&&... args)
+            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
+
+            template<typename... ArgTypes>
+            auto // std::invoke_result_t<T const&, ArgTypes...>
+            operator()(ArgTypes&&... args) const
+            { return std::invoke(ref(), std::forward<ArgTypes>(args)...); }
 
         private:
             T m_t;
