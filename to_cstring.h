00746b0d7fa31452a23f2e038cc72d34656c0ad7
Nevin Liber <nevin@cplusplusguy.com>
2018-05-11 02:03:43 -0500

    Templated version of to_cstring
    TODO rename the namespace

--- to_cstring.h
+++ to_cstring.h
@@ -1,28 +1,32 @@
-#ifndef COOL_TO_CSTRING_H_
-#define COOL_TO_CSTRING_H_
+#ifndef COOL2_TO_CSTRING_H_
+#define COOL2_TO_CSTRING_H_
 
 #include <algorithm>    // copy
 #include <array>
 #include <cassert>
 #include <climits>      // CHAR_BIT
 #include <cstddef>      // size_t, ptrdiff_t
-#include <cstdint>      // uintmax_t
+#include <cstdint>      // uint_fast*_t
 #include <iterator>     // reverse_iterator
 #include <ostream>
 #include <string>       // char_traits
 #include <string_view>
+#include <tuple>
 #include <type_traits>
 #include <utility>      // swap
 
-namespace cool
+namespace cool2
 {
+    template<typename...>
+    class to_cstring;
+
     ///////////////////////////////////////////////////////////////////////////
-    // to_cstring
+    // to_cstring<I, integral_constant<int, Base>>`
     //
     //  to_cstring is a non-allocating owning string
     //  which holds an integral value converted to a character string.
     //
-    //  Requires that base has a value between 2 and 36 (inclusive).
+    //  Requires that base has a value between Base and 36 (inclusive).
     //
     //  On construction, the value is converted to a string of digits in the
     //  given base (with no redundant leading zeroes).  Digits in the range
@@ -30,13 +34,21 @@ namespace cool
     //  the value is less than zero, the representation starts with '-'.
     //
     //  One example of using this is printf("%s\n", to_cstring(value).c_str());
-    //  * It doesn't use locales, so it may be faster.
     //  * Like streams, you don't have to know the exact type of value passed
     //    to it, as a templated constructor takes care of that
     //
     ///////////////////////////////////////////////////////////////////////////
-    class to_cstring
+    // In general, we can only stream temporaries
+    template<typename... Ts>
+    std::ostream& operator<<(std::ostream& os, to_cstring<Ts...> const&& that)
+    { return os << std::move(that).c_str(); }
+
+    template<typename I, int Base>
+    class to_cstring<I, std::integral_constant<int, Base>>
     {
+        static_assert(std::is_integral_v<I> || std::is_same_v<I, __int128_t> || std::is_same_v<I, __uint128_t>);
+        static_assert(2 <= Base && Base <= 36);
+
     public:
         // types
         using value_type                      = char;
@@ -53,13 +65,15 @@ namespace cool
         using difference_type                 = std::ptrdiff_t;
         static const constexpr size_type npos = static_cast<size_type>(-1);
 
+        using element_type                    = I;
+        static const constexpr int min_base   = Base;
+
         // public constructors
-        template<typename I, typename = std::enable_if_t<std::is_integral_v<I>>>
-        explicit to_cstring(I i, int base = 10) noexcept
+        explicit to_cstring(I i, int base = Base) noexcept
         : to_cstring{i < I{}, magnitude_type(i < I{} ? -i : i), magnitude_type(base)}
         {}
 
-        // Copying added for efficiency (compiler-generated copy/move are correct but inefficient)
+        // Copying added for efficiency (compiler-generated copy/move are correct but may copy more stuff))
         to_cstring(to_cstring const& that) noexcept
         : m_pos{that.m_pos}
         {
@@ -93,8 +107,24 @@ namespace cool
         // capacity
         size_type size()                const noexcept { return max_size() - m_pos; }
         size_type length()              const noexcept { return size(); }
-        static constexpr size_type max_size() noexcept { return m_max_size; }
         static constexpr bool      empty()    noexcept { return false; }
+        static constexpr size_type max_size() noexcept
+        {
+            // size starts out as is_signed, thus making room for '-' when true
+            // magnitude starts out as the max magnitude: if is_signed, then 0x800... else 0xfff...
+            // Run the same algorithm for counting bytes needed for is_signed + max magnitude
+            // as to_cstring constructor uses to assign the chars
+            // Assumes 2s complement
+            size_type      size{I(-1) < I{}};
+            magnitude_type magnitude{static_cast<magnitude_type>(size ? magnitude_type(1) << (CHAR_BIT * sizeof(I) - 1) : magnitude_type(I(-1)))};
+            do
+            {
+                ++size;
+                magnitude /= magnitude_type{Base};
+            } while (magnitude);
+
+            return size;
+        }
 
         // element access
         const_reference operator[](size_type pos) const          { return m_cstring[m_pos + pos]; }
@@ -151,18 +181,30 @@ namespace cool
             swap(*this, that);
         }
 
+        // We can stream this even if it isn't a temporary
         friend std::ostream& operator<<(std::ostream& os, to_cstring const& that)
         { return os << that.c_str(); }
 
     private:
-        using magnitude_type                        = std::uintmax_t;
-        static const constexpr size_type m_max_size = sizeof('-') + CHAR_BIT * sizeof(magnitude_type);
-        using cstring_type                          = std::array<char, m_max_size + sizeof('\0')>;
+        // Unsigned type for performing I to cstring
+        // Indexed by sizeof(I)
+        using magnitude_type = std::tuple_element_t<sizeof(I), std::tuple<void
+            , uint_fast8_t
+            , uint_fast16_t
+            , uint_fast32_t, uint_fast32_t
+            , uint_fast64_t, uint_fast64_t, uint_fast64_t, uint_fast64_t
+            , __uint128_t, __uint128_t, __uint128_t, __uint128_t, __uint128_t, __uint128_t, __uint128_t, __uint128_t
+        >>;
 
+        // cstring_type is large enough to hold max_size() chars and the trailing '\0'
+        using cstring_type = std::array<char, max_size() + sizeof('\0')>;
+
+        // Private because this can represent '-0', as well as magnitudes which aren't
+        // guaranteed to fit in cstring_type
         explicit to_cstring(bool negative, magnitude_type magnitude, magnitude_type base) noexcept
         : m_pos{max_size()}
         {
-            assert(2 <= base && base <= 36);
+            assert(Base <= base && base <= 36);
 
             m_cstring[m_pos] = '\0';
             do
@@ -180,7 +222,51 @@ namespace cool
         size_type    m_pos;
     };
 
-}  // cool namespace
+    // Single parameter constructor assumes base 10
+    template<typename I, typename = std::enable_if_t<std::is_integral_v<I> || std::is_same_v<I, __int128_t> || std::is_same_v<I, __uint128_t>>>
+    explicit to_cstring(I) -> to_cstring<I, std::integral_constant<int, 10>>;
 
-#endif /* COOL_TO_CSTRING_H_ */
+    // Two parameter constructor uses the one that can hold the largest magnitude, which is base 2
+    template<typename I, typename = std::enable_if_t<std::is_integral_v<I> || std::is_same_v<I, __int128_t> || std::is_same_v<I, __uint128_t>>>
+    explicit to_cstring(I, int) -> to_cstring<I, std::integral_constant<int, 2>>;
+
+
+    // Extend to wrapping std::string so it has a consistent interface
+    template<>
+    class to_cstring<std::string>
+    {
+    public:
+        explicit to_cstring(std::string const& s) noexcept : m_s{s} {}
+
+        // Only called on a temporary
+        const char* c_str() const && noexcept { return m_s.c_str(); }
+
+    private:
+        std::string const& m_s;
+    };
+
+    explicit to_cstring(std::string const&) -> to_cstring<std::string>;
+
+
+    // Extend to wrapping const char*
+    // Checks for nullptr
+    template<>
+    class to_cstring<const char*>
+    {
+    public:
+        explicit to_cstring(const char* s) noexcept : m_s{s} {}
+
+        // Only called on a temporary
+        const char* c_str() const && noexcept { return m_s ? m_s : "nullptr"; }
+
+    private:
+        const char* m_s;
+    };
+
+    explicit to_cstring(const char*) -> to_cstring<const char*>;
+
+}  // cool2 namespace
+
+
+#endif /* COOL2_TO_CSTRING_H_ */
 
