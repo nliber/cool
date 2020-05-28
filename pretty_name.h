482834bf942e79ef9f5b7abafe3488b314b0e1eb
Nevin Liber <nevin@cplusplusguy.com>
2020-05-28 16:02:41 -0500

    Fixed the documentation in pretty_name.h

--- pretty_name.h
+++ pretty_name.h
@@ -20,32 +20,39 @@ namespace cool
     //  pretty_type is a function template that encapsulates the human readable
     //  name of the type specified.
     //
+    //  In conjunction with the pretty_type_t constructor, pretty_type does the
+    //  work for all the rest of the function templates.
+    //
     // pretty_ref
     //
-    //  template<typename T> pretty_lref(T&&)
+    //  template<typename T> pretty_ref(T&&)
     //  pretty_lref is a function template that encapsulates the human readable
     //  name of the (static) type deduced.  The result is always
-    //  reference-qualified.  This is how forwarding references deduce.
+    //  reference-qualified.
+    //
+    //  This shows the reference type that forward<T> returns.
     //
     // pretty_lref
     //
     //  template<typename T> pretty_lref(T&&)
     //  pretty_lref is a function template that encapsulates the human readable
-    //  name of the (static) type deduced.  Only if an l-value is passed in
-    //  is the result reference-qualified.
+    //  name of the (static) type deduced.  The result is reference-qualified
+    //  if and only if an l-value reference is passed in.
+    //
+    //  This is primarily used to show how forwarding references are deduced.
     //
     // pretty_type_t
     // 
     //  pretty_type_t is a concrete class publicly derived from string_view
-    //  which pretty_name, pretty_type and pretty_lref return.
+    //  which the pretty_* function templates return.
     //
-    //  It is necessary because starting with gcc 10 alias templates such as
+    //  It is necessary because starting with gcc10 alias templates such as
     //  string_view are also expanded as part of __PRETTY_FUNCTION__ and
-    //  would make parsing __PRETTY_FUNCTION__ more complicated.
+    //  would make parsing __PRETTY_FUNCTION__ more complicated and the
+    //  __PRETTY_FUNCTION__ strings stored in the binary even bigger.
     //
-    //  Unlike string_view in general, pretty_type_t never holds a dangling
-    //  reference.
-    //
+    //  Unlike string_view in general, pretty_type_t never can hold a dangling
+    //  reference, as the __PRETTY_FUNCTION__ strings have program lifetime.
     ///////////////////////////////////////////////////////////////////////////
     
     class pretty_type_t : public std::string_view
