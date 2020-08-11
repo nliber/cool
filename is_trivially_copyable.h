078407a060f5123ec760c212645f6cbee777fa5a
Nevin Liber <nliber@anl.gov>
2020-08-11 01:44:03 -0500

    Better checking for is_trivially_copyable

--- is_trivially_copyable.h
+++ is_trivially_copyable.h
@@ -19,34 +19,69 @@ namespace cool
     template<typename T>
     constexpr void is_trivially_copyable() noexcept
     {
+        // Both clang and gcc have a bug where they don't take into account
+        // the trivial, non-deleted destructor
+        constexpr bool tc = std::is_trivially_copyable<T>::value &&
+                            std::is_trivially_destructible<T>::value;
+
+        constexpr bool cc = std::is_copy_constructible<T>::value;
+        constexpr bool mc = std::is_move_constructible<T>::value;
+        constexpr bool ca = std::is_copy_assignable<T>::value;
+        constexpr bool ma = std::is_move_assignable<T>::value;
+        constexpr bool d  = std::is_destructible<T>::value;
+
+        constexpr bool tcc = std::is_trivially_copy_constructible<T>::value;
+        constexpr bool tmc = std::is_trivially_move_constructible<T>::value;
+        constexpr bool tca = std::is_trivially_copy_assignable<T>::value;
+        constexpr bool tma = std::is_trivially_move_assignable<T>::value;
+        constexpr bool td  = std::is_trivially_destructible<T>::value;
+
+        constexpr bool cmca = cc || mc || ca || ma;
+
         static_assert(std::is_trivially_copyable<T>::value,
-                     "not trivially copyable");
+                      "not trivially copyable");
 
-        static_assert(std::is_copy_constructible<T>::value ||
-                      std::is_move_constructible<T>::value ||
-                      std::is_copy_assignable<T>::value    ||
-                      std::is_move_assignable<T>::value    ,
-                     "copy/move constructors/assignment operators deleted");
+        // has at least one eligible copy constructor, move constructor,
+        // copy assignment operator, or move assignment operator
 
-        static_assert(std::is_trivially_copy_constructible<T>::value ||
-                     !std::is_copy_constructible<T>::value           ,
-                     "not trivially copy constructible");
+        static_assert(cmca,
+                      "deleted copy/move constructors/assignment operators");
 
-        static_assert(std::is_trivially_move_constructible<T>::value ||
-                     !std::is_move_constructible<T>::value           ,
-                     "not trivially move constructible");
+        //each eligible copy constructor, move constructor,
+        //copy assignment operator, and move assignment
 
-        static_assert(std::is_trivially_copy_assignable<T>::value ||
-                     !std::is_copy_assignable<T>::value           ,
-                     "not trivially copy assignable");
+        static_assert(tcc || !cc,
+                      "non-trivial copy constructor");
 
-        static_assert(std::is_trivially_move_assignable<T>::value ||
-                     !std::is_move_assignable<T>::value           ,
-                     "not trivially move assignable");
+        static_assert(cc || !cmca || tc,
+                      "deleted copy constructor");
 
-        static_assert(std::is_trivially_destructible<T>::value,
-                     "not trivially destructible");
+        static_assert(tmc || !mc,
+                      "non-trivial move constructor");
 
+        static_assert(mc || !cmca || tc,
+                      "deleted move constructor");
+
+        static_assert(tca || !ca,
+                      "non-trivial copy assignment operator");
+
+        static_assert(ca || !cmca || tc,
+                      "deleted copy assignment operator");
+
+        static_assert(tma || !ma,
+                      "non-trivial move assignment operator");
+
+        static_assert(ma || !cmca || tc,
+                      "deleted move assignment operator");
+
+
+        // has a trivial, non-deleted destructor
+       
+        static_assert(td || !d,
+                      "non-trivial destructor");
+
+        static_assert(d,
+                      "deleted destructor");
     }
 
 } // cool namespace
