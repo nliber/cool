82e706f635e36dd3dbdde09fab425104d5b288d6
Nevin ":-)" Liber <nliber+github@gmail.com>
2023-07-25 17:03:23 -0500

    Added comment about inline variables

--- is_scoped_enum.h
+++ is_scoped_enum.h
@@ -16,12 +16,9 @@ namespace cool {
 ///////////////////////////////////////////////////////////////////////////
 
 #if __cplusplus >= 202002
-
 using std::underlying_type;
 using std::underlying_type_t;
-
 #else
-
 namespace detail {
 template <typename E, bool = std::is_enum<E>::value>
 struct underlying_type {};
@@ -37,7 +34,6 @@ using underlying_type = detail::underlying_type<E>;
 
 template <typename E>
 using underlying_type_t = typename underlying_type<E>::type;
-
 #endif
 
 ///////////////////////////////////////////////////////////////////////////
@@ -48,16 +44,12 @@ using underlying_type_t = typename underlying_type<E>::type;
 ///////////////////////////////////////////////////////////////////////////
 
 #if defined(__cpp_lib_to_underlying)
-
 using std::to_underlying;
-
 #else
-
 template <typename E>
 constexpr underlying_type_t<E> to_underlying(E e) noexcept {
-    return static_cast<underlying_type_t<E> >(e);
+    return static_cast<underlying_type_t<E>>(e);
 }
-
 #endif
 
 ///////////////////////////////////////////////////////////////////////////
@@ -71,12 +63,9 @@ constexpr underlying_type_t<E> to_underlying(E e) noexcept {
 ///////////////////////////////////////////////////////////////////////////
 
 #if defined(__cpp_lib_is_scoped_enum)
-
 using std::is_scoped_enum;
 using std::is_scoped_enum_v;
-
 #else
-
 namespace detail {
 template <typename E, bool = std::is_enum<E>::value>
 struct is_scoped_enum : std::false_type {};
@@ -84,15 +73,18 @@ struct is_scoped_enum : std::false_type {};
 template <typename E>
 struct is_scoped_enum<E, true>
     : std::integral_constant<
-          bool, !std::is_convertible<E, underlying_type_t<E> >::value> {};
+          bool, !std::is_convertible<E, underlying_type_t<E>>::value> {};
 }  // namespace detail
 
 template <typename E>
-using is_scoped_enum = typename detail::is_scoped_enum<E>::type;
+struct is_scoped_enum : detail::is_scoped_enum<E>::type {};
 
+// Technically, this needs C++17 and __cpp_inline_variables,
+// but it compiles with clang++-11 and g++-11 in C++11 mode
+//#if defined(__cpp_inline_variables)
 template <typename E>
 inline constexpr bool is_scoped_enum_v = is_scoped_enum<E>::value;
-
+//#endif
 #endif
 
 }  // namespace cool
